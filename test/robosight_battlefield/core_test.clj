4ef716d0b5b1cdabc699392164071961a40d5ba2
OJIMA Ryoji <rojima1@gmail.com>
2017-04-12 12:01:22 +0900

    refactored.

--- test/robosight_battlefield/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns robosight-battlefield.core-test
-  (:require [clojure.test :refer :all]
-            [robosight-battlefield.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

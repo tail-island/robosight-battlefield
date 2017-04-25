25cc7be29061273818d1d2a6069dfa251f6898b0
OJIMA Ryoji <rojima1@gmail.com>
2017-04-25 09:55:39 +0900

    refactored.

--- src/robosight/battlefield.clj
+++ src/robosight/battlefield.clj
@@ -8,42 +8,36 @@
   (:gen-class :name com.tail_island.robosight.Battlefield
               :main true))
 
-(defn- print-winner
-  [winner]
-  (println (pr-str (if winner
-                     (format "%s team win!" (["Left" "Right"] winner))
-                     "No game.."))))
-
 (defn -main
   [& args]
   (let [processes (doall (map (fn [[cmd dir]]
                                 (.exec (Runtime/getRuntime) cmd nil (io/as-file dir)))
                               (partition 2 args)))
-        ins       (doall (map #(PrintWriter. (io/writer (.getOutputStream %) :encoding (System/getProperty "file.encoding"))) processes))
-        outs      (doall (map #(io/reader               (.getInputStream  %) :encoding (System/getProperty "file.encoding"))  processes))
-        errs      (doall (map #(io/reader               (.getErrorStream  %) :encoding (System/getProperty "file.encoding"))  processes))
+        encoding  (System/getProperty "file.encoding")
+        ins       (map #(PrintWriter. (io/writer (.getOutputStream %) :encoding encoding)) processes)
+        outs      (map #(io/reader               (.getInputStream  %) :encoding encoding)  processes)
+        errs      (map #(io/reader               (.getErrorStream  %) :encoding encoding)  processes)
         tick      (robosight/tick-fn ins outs)]
     (try
       (doseq [err errs]
-        (binding [*out* *err*]
-          (async/go-loop []
-            (when-let [s (.readLine err)]
-              (println s)
-              (recur)))))
+        (async/go-loop []
+          (when-let [s (.readLine err)]
+            (binding [*out* *err*]
+              (println s))
+            (recur))))
       ((fn [state]
-         (println (pr-str state))
+         (println (pr-str {:state state}))
          (if (robosight/game-finished? state)
-           (print-winner (robosight/winner state))
+           (println (pr-str {:winner (robosight/winner state)}))
            (recur (tick state))))
        robosight/initial-state)
-
       (catch clojure.lang.ExceptionInfo ex
         (case (:reason (ex-data ex))
-          :timeout (print-winner (Math/abs (- (:timeout-team (ex-data ex)) 1)))
+          :timeout (println (pr-str {:winner (Math/abs (- (:timeout-team (ex-data ex)) 1))
+                                     :note   :timeout}))
           (.printStackTrace ex))
         (doseq [process processes]
           (.destroy process)))
-
       (finally
         (doseq [closeable (concat ins outs errs)]
           (.close closeable))

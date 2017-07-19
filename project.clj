4c4ae68d83e6d556418013d1534ac6973cdedabb
OJIMA Ryoji <rojima1@gmail.com>
2017-07-19 20:37:28 +0900

    Logging function is added. Practice mode is added.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure            "1.8.0"]
                  [org.clojure/core.async         "0.3.442"]
+                 [org.clojure/tools.logging      "0.4.0"]
                  [com.tail-island/robosight-core "0.1.0"]]
   :aot          :all
   :main         com.tail_island.robosight.Battlefield
--- src/robosight/battlefield.clj
+++ src/robosight/battlefield.clj
@@ -1,7 +1,8 @@
 (ns robosight.battlefield
-  (:require   (clojure.core [async :as async :refer [>!! <!! >! <!]])
-              (clojure.java [io    :as io])
-              (robosight    [core  :as robosight]))
+  (:require   (clojure.core  [async   :as async :refer [>!! <!! >! <!]])
+              (clojure.java  [io      :as io])
+              (clojure.tools [logging :as logging])
+              (robosight     [core    :as robosight]))
   (:import    (clojure.lang         ExceptionInfo)
               (java.io              PrintWriter)
               (java.util.concurrent TimeUnit))
@@ -10,38 +11,38 @@
 
 (defn -main
   [& args]
-  (let [processes (doall (map (fn [[cmd dir]]
-                                (.exec (Runtime/getRuntime) cmd nil (io/as-file dir)))
-                              (partition 2 args)))
-        encoding  (System/getProperty "file.encoding")
-        ins       (map #(PrintWriter. (io/writer (.getOutputStream %) :encoding encoding)) processes)
-        outs      (map #(io/reader               (.getInputStream  %) :encoding encoding)  processes)
-        errs      (map #(io/reader               (.getErrorStream  %) :encoding encoding)  processes)
-        tick      (robosight/tick-fn ins outs)]
-    (try
-      (doseq [err errs]
-        (async/go-loop []
-          (when-let [s (.readLine err)]
-            (binding [*out* *err*]
-              (println s))
-            (recur))))
-      ((fn [state]
-         (println (pr-str {:state state}))
-         (if (robosight/game-finished? state)
-           (println (pr-str {:winner (robosight/winner state)}))
-           (recur (tick state))))
-       robosight/initial-state)
-      (catch clojure.lang.ExceptionInfo ex
-        (case (:reason (ex-data ex))
-          :timeout (println (pr-str {:winner (Math/abs (- (:timeout-team (ex-data ex)) 1))
-                                     :note   :timeout}))
-          (.printStackTrace ex))
-        (doseq [process processes]
-          (.destroy process)))
-      (finally
-        (doseq [closeable (concat ins outs errs)]
-          (.close closeable))
-        (doseq [process processes]
-          (.waitFor process 1000 TimeUnit/MILLISECONDS)
-          (.destroy process))
-        (shutdown-agents)))))
+  (binding [robosight/*practice-mode* (System/getenv "ROBOSIGHT_PRACTICE_MODE")]
+    (let [processes (doall (map (fn [[cmd dir]]
+                                  (.exec (Runtime/getRuntime) cmd nil (io/as-file dir)))
+                                (partition 2 args)))
+          encoding  (System/getProperty "file.encoding")
+          ins       (map #(PrintWriter. (io/writer (.getOutputStream %) :encoding encoding)) processes)
+          outs      (map #(io/reader               (.getInputStream  %) :encoding encoding)  processes)
+          errs      (map #(io/reader               (.getErrorStream  %) :encoding encoding)  processes)
+          tick      (robosight/tick-fn ins outs)]
+      (try
+        (doseq [[team err] (map-indexed vector errs)]
+          (async/go-loop []
+            (when-let [s (.readLine err)]
+              (logging/infof "team %d, stderr is '%s'." team s)
+              (recur))))
+        ((fn [state]
+           (println (pr-str {:state state}))
+           (if (robosight/game-finished? state)
+             (println (pr-str {:winner (robosight/winner state)}))
+             (recur (tick state))))
+         robosight/initial-state)
+        (catch clojure.lang.ExceptionInfo ex
+          (case (:reason (ex-data ex))
+            :timeout (println (pr-str {:winner (Math/abs (- (:timeout-team (ex-data ex)) 1))
+                                       :note   :timeout}))
+            (.printStackTrace ex))
+          (doseq [process processes]
+            (.destroy process)))
+        (finally
+          (doseq [closeable (concat ins outs errs)]
+            (.close closeable))
+          (doseq [process processes]
+            (.waitFor process 1000 TimeUnit/MILLISECONDS)
+            (.destroy process))
+          (shutdown-agents))))))
